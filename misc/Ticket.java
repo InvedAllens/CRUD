3111d241f80fcb91359b9049d28bdc1e143ef1cc
rubaanxd <teckever6@hotmail.com>
2019-11-20 12:43:41 -0600

    Se agrego clase ticket para almacenar de manera local y temporal los datos del Ticket

---
+++ misc/Ticket.java
@@ -0,0 +1,115 @@
+package misc;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Ruben Angeles
+ */
+public class Ticket {
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the ticket
+     */
+    public int getTicket() {
+        return ticket;
+    }
+
+    /**
+     * @param ticket the ticket to set
+     */
+    public void setTicket(int ticket) {
+        this.ticket = ticket;
+    }
+
+    /**
+     * @return the ns
+     */
+    public String getNs() {
+        return ns;
+    }
+
+    /**
+     * @param ns the ns to set
+     */
+    public void setNs(String ns) {
+        this.ns = ns;
+    }
+
+    /**
+     * @return the detalle
+     */
+    public String getDetalle() {
+        return detalle;
+    }
+
+    /**
+     * @param detalle the detalle to set
+     */
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    /**
+     * @return the observaciones
+     */
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    /**
+     * @param observaciones the observaciones to set
+     */
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    /**
+     * @return the path
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * @param path the path to set
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    private int id;
+    private int ticket;
+    private String ns;
+    private String detalle;
+    private String observaciones;
+    private String path;
+    private Date fecha;
+}
